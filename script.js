a42e20ec2a585f3ee8b96b6f16c07345d28c5399
Febe Apaya <fap23506@my.hghs.school.nz>
2025-06-09 17:42:44 +1200

    9/6/25
    I just added the portfolio gallery

---
+++ script.js
@@ -0,0 +1,18 @@
+// gallery pop up functionality //
+const galleryPopup = document.getElementById("gallery-popup");
+const galleryPopupImg = document.getElementById("gallery-popup-img");
+const galleryImages = document.querySelectorAll(".portfolio img");
+const galleryClose = document.querySelector(".gallery-close");
+
+// show gallery-popup when any image is clicked
+galleryImages.forEach(img => {
+  img.addEventListener("click", () => {
+    galleryPopup.style.display = "flex";
+    galleryPopupImg.src = img.src;
+  });
+});
+
+// hide gallery-popup when close button is clicked
+galleryClose.addEventListener("click", () => {
+  galleryPopup.style.display = "none";
+});
